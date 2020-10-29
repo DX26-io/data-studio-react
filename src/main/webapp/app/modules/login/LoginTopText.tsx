ca3ec1b279a4dd51dcb9bfd056366dfee14dc0fd
dev-fe-dx26 <dev-fe-dx26>
2020-10-29 12:21:01 +0530

    removed comments

--- src/main/webapp/app/modules/login/LoginTopText.tsx
+++ src/main/webapp/app/modules/login/LoginTopText.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
-// TODO: Test Cases
-// TODO : year should come dynamically 
-
 const LoginTopText = props => (
     <Typography variant="h6" className="dx26-font-bold" style={{ position: 'absolute', right: "3%", top: "4%" }}>
         DX26
