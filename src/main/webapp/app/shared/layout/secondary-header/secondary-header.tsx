c5dfdbeffec9c2048eebf93f10c0d8544b7bde8e
dev-fe-dx26 <dev-fe-dx26>
2020-11-21 15:52:14 +0530

    secondary header breadcrumb issue

--- src/main/webapp/app/shared/layout/secondary-header/secondary-header.tsx
+++ src/main/webapp/app/shared/layout/secondary-header/secondary-header.tsx
@@ -20,15 +20,13 @@ const SecondaryHeader: React.FC<PropsWithChildren<ISecondaryHeaderProps>> = prop
   return (
     <View paddingX="size-150" paddingY="size-100" backgroundColor="default" borderBottomWidth={'thin'} borderBottomColor={'default'}>
       <Grid areas={['breadcrumbs title options']} columns={['1fr', '1fr', '1fr']} maxHeight={'size-600'}>
-        <Flex gridArea="breadcrumbs" justifyContent="start" alignItems="center">
-          <Breadcrumbs size="M">
-            {props.breadcrumbItems.map(item => (
-              <Item key={item.key}>
-                <Link to={item.route}>{item.label}</Link>
-              </Item>
-            ))}
-          </Breadcrumbs>
-        </Flex>
+        <Breadcrumbs size="M">
+          {props.breadcrumbItems.map(item => (
+            <Item key={item.key}>
+              <Link to={item.route}>{item.label}</Link>
+            </Item>
+          ))}
+        </Breadcrumbs>
         <Flex gridArea="title" justifyContent="center" alignItems="center">
           <Heading level={3} marginY="size-0">
             {props.title}
