8f09d4b2f10eb368adf5cc019b865b149e39b3bd
karthik-reddivari-dx26 <karthik@dx26.io>
2020-11-24 22:38:46 +0100

    Added translations to login componennt

--- src/main/webapp/app/modules/login/login-form.tsx
+++ src/main/webapp/app/modules/login/login-form.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Checkbox, Flex, Text, TextField, View, Link, Form } from '@adobe/react-spectrum';
 import Alert from '@spectrum-icons/workflow/Alert';
+import { Translate } from 'react-jhipster';
 
 export interface ILoginProps {
   loginError: boolean;
@@ -29,7 +30,9 @@ export const LoginForm = (props: ILoginProps) => {
       <Form width="size-4600" isRequired isQuiet onSubmit={handleSubmit} data-testid="login-form">
         <View marginBottom="size-600">
           <span className="spectrum-Heading spectrum-Heading--sizeXXL">
-            <Text>Login</Text>
+            <Text>
+              <Translate contentKey="login.title">Login</Translate>
+            </Text>
           </span>
         </View>
         <TextField
@@ -55,7 +58,7 @@ export const LoginForm = (props: ILoginProps) => {
             <Alert color="negative" />
             <Text marginBottom="size-300">
               <span className="spectrum-Body-emphasis error-message" data-testid="login-error">
-                Your username or password does not match any account
+                <Translate contentKey="login.messages.error.authentication">Your username or password does not match any account</Translate>
               </span>
             </Text>
           </Flex>
@@ -65,20 +68,22 @@ export const LoginForm = (props: ILoginProps) => {
             <Alert color="negative" />
             <Text marginBottom="size-300">
               <span className="spectrum-Body-emphasis error-message" data-testid="empty-error">
-                Username or password should not be empty
+                <Translate contentKey="login.messages.error.emptyUsernameOrPassword"> Username or password should not be empty</Translate>
               </span>
             </Text>
           </Flex>
         )}
         <Checkbox marginTop="static-size-25" isRequired={false} isSelected={rememberMe} onChange={setRememberMe} data-testid="remember-me">
-          Remember me
+          <Translate contentKey="login.form.rememberMe">Remember me</Translate>
         </Checkbox>
         <Flex data-testid="login-action" marginTop="size-400" alignItems="center" justifyContent="center" direction="row-reverse">
           <Button data-testid="submit" variant="cta" marginStart="auto" type="submit">
-            Sign In
+            <Translate contentKey="login.form.button">Sign In</Translate>
           </Button>
           <Link isQuiet={true}>
-            <a href="/">Use Single Sign On (SSO)</a>
+            <a href="/">
+              <Translate contentKey="login.form.sso">Use Single Sign On (SSO)</Translate>
+            </a>
           </Link>
         </Flex>
       </Form>
