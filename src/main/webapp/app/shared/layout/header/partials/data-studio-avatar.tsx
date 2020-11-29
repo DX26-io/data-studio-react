99a5948b28feacf6e8848d8c515e747407e6a514
karthik-reddivari-dx26 <karthik@dx26.io>
2020-11-29 01:01:50 +0100

    Removed material avatar and replaced it with spectrum icon

--- src/main/webapp/app/shared/layout/header/partials/data-studio-avatar.tsx
+++ src/main/webapp/app/shared/layout/header/partials/data-studio-avatar.tsx
@@ -1,33 +1,19 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { Button, Divider, Flex, Text, View } from '@adobe/react-spectrum';
-import { Avatar } from '@material-ui/core';
 import HeaderPopover from 'app/shared/layout/header/partials/header-popover';
 import { Translate } from 'react-jhipster';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { IRootState } from 'app/shared/reducers';
+import User from '@spectrum-icons/workflow/User';
 
 const DataStudioAvatar: React.FC = () => {
-  const avatarStyles = makeStyles(theme => ({
-    small: {
-      width: theme.spacing(4),
-      height: theme.spacing(4),
-    },
-  }));
-  const avatarClasses = avatarStyles();
   const history = useHistory();
   const account = useSelector((storeState: IRootState) => storeState.authentication.account);
 
   return (
     <>
-      <HeaderPopover
-        icon={
-          <Avatar className={avatarClasses.small}>
-            <span className="avatar-initial">H</span>
-          </Avatar>
-        }
-      >
+      <HeaderPopover icon={<User />}>
         <View padding="size-300">
           <Flex alignItems="center" justifyContent="center" direction="column">
             <span className="spectrum-Body spectrum-Body--L">
