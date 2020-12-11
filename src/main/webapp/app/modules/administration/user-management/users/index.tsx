bff08f90c1c1bfde81ead1e8f176d263fe686e52
dev-fe-dx26 <dev-fe-dx26>
2020-12-11 12:24:33 +0530

    removed unnecessary code-2

--- src/main/webapp/app/modules/administration/user-management/users/index.tsx
+++ src/main/webapp/app/modules/administration/user-management/users/index.tsx
@@ -3,9 +3,6 @@ import { Switch } from 'react-router-dom';
 
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 import UserManagementUsers from '../users/user-management-users';
-import UserManagementDetail from '../users/user-management-detail';
-import UserManagementUpdate from '../users/user-management-update';
-import UserManagementDeleteDialog from './user-management-delete-dialog';
 
 const Routes = ({ match }) => (
   <div>
