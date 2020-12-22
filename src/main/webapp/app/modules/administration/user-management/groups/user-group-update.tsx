3c58fd759f5bff44b8540671d4060bba45802bbc
dev-fe-dx26 <dev-fe-dx26>
2020-12-22 09:19:01 +0530

    removed loading from user-update and user-update-group

--- src/main/webapp/app/modules/administration/user-management/groups/user-group-update.tsx
+++ src/main/webapp/app/modules/administration/user-management/groups/user-group-update.tsx
@@ -76,104 +76,98 @@ export const UserUpdate = (props: IUserGroupUpdateProps) => {
   };
 
   return (
-    <div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <Dialog data-testid="group-form-dialog">
-          <Heading>
-            <Flex alignItems="center" gap="size-100" data-testid="group-form-heading">
-              {!isNew ? (
-                <Translate contentKey="userGroups.home.editLabel">Edit Group</Translate>
-              ) : (
-                <Translate contentKey="userGroups.home.createLabel">Create Group</Translate>
-              )}
+    <Dialog data-testid="group-form-dialog">
+      <Heading>
+        <Flex alignItems="center" gap="size-100" data-testid="group-form-heading">
+          {!isNew ? (
+            <Translate contentKey="userGroups.home.editLabel">Edit Group</Translate>
+          ) : (
+            <Translate contentKey="userGroups.home.createLabel">Create Group</Translate>
+          )}
+        </Flex>
+      </Heading>
+      <Header data-testid="group-form-action">
+        <Flex alignItems="center" gap="size-100">
+          <Button variant="secondary" onPress={handleClose} data-testid="group-form-cancel">
+            <Translate contentKey="entity.action.cancel">Cancel</Translate>
+          </Button>
+          <Button variant="cta" onPress={saveUser} isDisabled={updating || !error.isValid} data-testid="group-form-submit">
+            <Translate contentKey="entity.action.save">Save</Translate>
+          </Button>
+        </Flex>
+      </Header>
+      <Divider />
+      <Content>
+        <Form data-testid="group-form">
+          <TextField
+            label="Name"
+            placeholder="ROLE_ACCOUNT"
+            type="text"
+            value={name}
+            onChange={setName}
+            autoFocus
+            isRequired
+            data-testid="name"
+          />
+          <div style={{ marginTop: '20px' }}>
+            <Flex alignItems="center" gap="size-25">
+              <p className="spectrum-Heading spectrum-Heading--sizeXXS">
+                <Translate contentKey="userGroups.tip"></Translate>
+              </p>
+              <AlertCircle size="S" />
             </Flex>
-          </Heading>
-          <Header data-testid="group-form-action">
-            <Flex alignItems="center" gap="size-100">
-              <Button variant="secondary" onPress={handleClose} data-testid="group-form-cancel">
-                <Translate contentKey="entity.action.cancel">Cancel</Translate>
-              </Button>
-              <Button variant="cta" onPress={saveUser} isDisabled={updating || !error.isValid} data-testid="group-form-submit">
-                <Translate contentKey="entity.action.save">Save</Translate>
-              </Button>
-            </Flex>
-          </Header>
-          <Divider />
-          <Content>
-            <Form data-testid="group-form">
-              <TextField
-                label="Name"
-                placeholder="ROLE_ACCOUNT"
-                type="text"
-                value={name}
-                onChange={setName}
-                autoFocus
-                isRequired
-                data-testid="name"
-              />
-              <div style={{ marginTop: '20px' }}>
-                <Flex alignItems="center" gap="size-25">
-                  <p className="spectrum-Heading spectrum-Heading--sizeXXS">
-                    <Translate contentKey="userGroups.tip"></Translate>
-                  </p>
-                  <AlertCircle size="S" />
-                </Flex>
-                {!isNew ? (
-                  <p>
-                    <span className="spectrum-Body-emphasis">
-                      <Translate contentKey="userGroups.updateTipMessage"></Translate>
-                    </span>
-                    <a className="dx26-link" onClick={redirectToPermissionPage}>
-                      &nbsp;&nbsp;
-                      <Translate contentKey="userGroups.permissionManagement"></Translate>
-                    </a>
-                  </p>
-                ) : (
-                  <p>
-                    <span className="spectrum-Body-emphasis">
-                      <Translate contentKey="userGroups.newTipMessage1"></Translate>
-                    </span>
-                    <a className="dx26-link" onClick={redirectToPermissionPage} data-testid="redirect">
-                      &nbsp;&nbsp;
-                      <Translate contentKey="userGroups.permissionManagement"></Translate>
-                    </a>
-                    &nbsp;&nbsp;
-                    <span className="spectrum-Body-emphasis">
-                      <Translate contentKey="userGroups.newTipMessage2"></Translate>
-                    </span>
-                  </p>
-                )}
-              </div>
-              {!isNew ? (
-                <React.Fragment>
-                  <span className="spectrum-Heading spectrum-Heading--sizeXXS">
-                    <Translate contentKey="entity.action.dangerZone">Danger Zone</Translate>
-                  </span>
-                  <Divider size="M" />{' '}
-                </React.Fragment>
-              ) : null}
-            </Form>
             {!isNew ? (
-              <Button data-testid="delete" variant="negative" onPress={removeGroup} marginTop="size-175">
-                <Translate contentKey="entity.action.delete">Delete</Translate>
-              </Button>
-            ) : null}
-            {!error.isValid && (
-              <Flex gap="size-100" data-testid="validation-error" marginTop="static-size-200">
-                <Alert color="negative" />
-                <Text marginBottom="size-300">
-                  <span className="spectrum-Body-emphasis error-message">
-                    <Translate contentKey={error.translationKey}></Translate>
-                  </span>
-                </Text>
-              </Flex>
+              <p>
+                <span className="spectrum-Body-emphasis">
+                  <Translate contentKey="userGroups.updateTipMessage"></Translate>
+                </span>
+                <a className="dx26-link" onClick={redirectToPermissionPage}>
+                  &nbsp;&nbsp;
+                  <Translate contentKey="userGroups.permissionManagement"></Translate>
+                </a>
+              </p>
+            ) : (
+              <p>
+                <span className="spectrum-Body-emphasis">
+                  <Translate contentKey="userGroups.newTipMessage1"></Translate>
+                </span>
+                <a className="dx26-link" onClick={redirectToPermissionPage} data-testid="redirect">
+                  &nbsp;&nbsp;
+                  <Translate contentKey="userGroups.permissionManagement"></Translate>
+                </a>
+                &nbsp;&nbsp;
+                <span className="spectrum-Body-emphasis">
+                  <Translate contentKey="userGroups.newTipMessage2"></Translate>
+                </span>
+              </p>
             )}
-          </Content>
-        </Dialog>
-      )}
-    </div>
+          </div>
+          {!isNew ? (
+            <React.Fragment>
+              <span className="spectrum-Heading spectrum-Heading--sizeXXS">
+                <Translate contentKey="entity.action.dangerZone">Danger Zone</Translate>
+              </span>
+              <Divider size="M" />{' '}
+            </React.Fragment>
+          ) : null}
+        </Form>
+        {!isNew ? (
+          <Button data-testid="delete" variant="negative" onPress={removeGroup} marginTop="size-175">
+            <Translate contentKey="entity.action.delete">Delete</Translate>
+          </Button>
+        ) : null}
+        {!error.isValid && (
+          <Flex gap="size-100" data-testid="validation-error" marginTop="static-size-200">
+            <Alert color="negative" />
+            <Text marginBottom="size-300">
+              <span className="spectrum-Body-emphasis error-message">
+                <Translate contentKey={error.translationKey}></Translate>
+              </span>
+            </Text>
+          </Flex>
+        )}
+      </Content>
+    </Dialog>
   );
 };
 
