78facafb65110817a124070ebd36af3f28626c8d
dev-fe-dx26 <dev-fe-dx26>
2021-02-04 11:05:44 +0530

    overloaded the function forwardCall

--- src/main/webapp/app/shared/websocket/proxy-websocket.service.ts
+++ src/main/webapp/app/shared/websocket/proxy-websocket.service.ts
@@ -1,10 +1,14 @@
 import { sendToWebSocket } from './stomp-client.service';
 
-const sendMsg = (sourceId: number, body: any, viewId: number) => {
+const sendMsg = (sourceId: number, body: any, viewId?: number) => {
   // console.log('sending message', body);
-  sendToWebSocket('/flair-ws/fbi-engine-grpc/' + sourceId + '/query/' + viewId, {}, JSON.stringify(body));
+  if (viewId) {
+    sendToWebSocket('/flair-ws/fbi-engine-grpc/' + sourceId + '/query/' + viewId, {}, JSON.stringify(body));
+  } else {
+    sendToWebSocket('/flair-ws/fbi-engine-grpc/' + sourceId + '/query', {}, JSON.stringify(body));
+  }
 };
 
-export const forwardCall = (sourceId: number, body: any, viewId: number) => {
+export const forwardCall = (sourceId: number, body: any, viewId?: number) => {
   sendMsg(sourceId, body, viewId);
 };
