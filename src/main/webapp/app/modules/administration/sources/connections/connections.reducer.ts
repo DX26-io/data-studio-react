2aed7b83fcf8ca4bbf0d32e30880ed67a929494a
dev-fe-dx26 <dev-fe-dx26>
2021-02-10 11:02:33 +0530

    removed commented code

--- src/main/webapp/app/modules/administration/sources/connections/connections.reducer.ts
+++ src/main/webapp/app/modules/administration/sources/connections/connections.reducer.ts
@@ -6,23 +6,12 @@ import { IUser, defaultValue } from 'app/shared/model/user.model';
 
 export const ACTION_TYPES = {
   FETCH_CONNECTIONS: 'connections/FETCH_CONNECTIONS',
-  FETCH_USERS: 'connections/FETCH_USERS',
-  FETCH_USER: 'connections/FETCH_USER',
-  CREATE_USER: 'connections/CREATE_USER',
-  UPDATE_USER: 'connections/UPDATE_USER',
-  DELETE_USER: 'connections/DELETE_USER',
-  RESET: 'connections/RESET',
 };
 
 const initialState = {
   loading: false,
   errorMessage: null,
-  users: [] as ReadonlyArray<IUser>,
   connections: [] as any[],
-  authorities: [] as any[],
-  user: defaultValue,
-  updating: false,
-  updateSuccess: false,
   totalItems: 0,
 };
 
@@ -36,78 +25,17 @@ export default (state: ConnectionsState = initialState, action): ConnectionsStat
         ...state,
         loading: true,
       };
-    // case REQUEST(ACTION_TYPES.FETCH_USERS):
-    // case REQUEST(ACTION_TYPES.FETCH_USER):
-    //   return {
-    //     ...state,
-    //     errorMessage: null,
-    //     updateSuccess: false,
-    //     loading: true,
-    //   };
-    // case REQUEST(ACTION_TYPES.CREATE_USER):
-    // case REQUEST(ACTION_TYPES.UPDATE_USER):
-    // case REQUEST(ACTION_TYPES.DELETE_USER):
-    //   return {
-    //     ...state,
-    //     errorMessage: null,
-    //     updateSuccess: false,
-    //     updating: true,
-    //   };
-    // case FAILURE(ACTION_TYPES.FETCH_USERS):
-    // case FAILURE(ACTION_TYPES.FETCH_USER):
     case FAILURE(ACTION_TYPES.FETCH_CONNECTIONS):
       return {
         ...state,
         loading: false,
         errorMessage: action.payload,
       };
-    // case FAILURE(ACTION_TYPES.CREATE_USER):
-    // case FAILURE(ACTION_TYPES.UPDATE_USER):
-    // case FAILURE(ACTION_TYPES.DELETE_USER):
-    //   return {
-    //     ...state,
-    //     loading: false,
-    //     updating: false,
-    //     updateSuccess: false,
-    //     errorMessage: action.payload,
-    //   };
     case SUCCESS(ACTION_TYPES.FETCH_CONNECTIONS):
       return {
         ...state,
         loading: false,
         connections: action.payload.data,
-      };
-    // case SUCCESS(ACTION_TYPES.FETCH_USERS):
-    //   return {
-    //     ...state,
-    //     loading: false,
-    //     users: action.payload.data,
-    //     totalItems: parseInt(action.payload.headers['x-total-count'], 10),
-    //   };
-    // case SUCCESS(ACTION_TYPES.FETCH_USER):
-    //   return {
-    //     ...state,
-    //     loading: false,
-    //     user: action.payload.data,
-    //   };
-    // case SUCCESS(ACTION_TYPES.CREATE_USER):
-    // case SUCCESS(ACTION_TYPES.UPDATE_USER):
-    //   return {
-    //     ...state,
-    //     updating: false,
-    //     updateSuccess: true,
-    //     user: action.payload.data,
-    //   };
-    // case SUCCESS(ACTION_TYPES.DELETE_USER):
-    //   return {
-    //     ...state,
-    //     updating: false,
-    //     updateSuccess: true,
-    //     user: defaultValue,
-    //   };
-    case ACTION_TYPES.RESET:
-      return {
-        ...initialState,
       };
     default:
       return state;
@@ -120,57 +48,3 @@ export const getConnections = () => ({
   type: ACTION_TYPES.FETCH_CONNECTIONS,
   payload: axios.get('api/connection'),
 });
-
-// Actions
-// export const getUsers: ICrudGetAllAction<IUser> = (page, size, sort) => {
-//   const requestUrl = `${apiUrl}${sort ? `?page=${page}&size=${size}&sort=${sort}` : ''}`;
-//   return {
-//     type: ACTION_TYPES.FETCH_USERS,
-//     payload: axios.get<IUser>(requestUrl),
-//   };
-// };
-
-// export const getRoles = () => ({
-//   type: ACTION_TYPES.FETCH_ROLES,
-//   payload: axios.get(`${apiUrl}/authorities`),
-// });
-
-// export const getUser: ICrudGetAction<IUser> = id => {
-//   const requestUrl = `${apiUrl}/${id}`;
-//   return {
-//     type: ACTION_TYPES.FETCH_USER,
-//     payload: axios.get<IUser>(requestUrl),
-//   };
-// };
-
-// export const createUser: ICrudPutAction<IUser> = user => async dispatch => {
-//   const result = await dispatch({
-//     type: ACTION_TYPES.CREATE_USER,
-//     payload: axios.post(apiUrl, user),
-//   });
-//   dispatch(getUsers());
-//   return result;
-// };
-
-// export const updateUser: ICrudPutAction<IUser> = user => async dispatch => {
-//   const result = await dispatch({
-//     type: ACTION_TYPES.UPDATE_USER,
-//     payload: axios.put(apiUrl, user),
-//   });
-//   dispatch(getUsers());
-//   return result;
-// };
-
-// export const deleteUser: ICrudDeleteAction<IUser> = id => async dispatch => {
-//   const requestUrl = `${apiUrl}/${id}`;
-//   const result = await dispatch({
-//     type: ACTION_TYPES.DELETE_USER,
-//     payload: axios.delete(requestUrl),
-//   });
-//   dispatch(getUsers());
-//   return result;
-// };
-
-// export const reset = () => ({
-//   type: ACTION_TYPES.RESET,
-// });
