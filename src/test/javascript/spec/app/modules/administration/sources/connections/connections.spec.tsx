1cf85df9e10600da0cb5f0b37cfa9d13265de652
dev-fe-dx26 <dev-fe-dx26>
2021-04-02 15:36:53 +0530

    fixed connections test case

--- src/test/javascript/spec/app/modules/administration/sources/connections/connections.spec.tsx
+++ src/test/javascript/spec/app/modules/administration/sources/connections/connections.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Connections, { IConnectionsProps } from 'app/modules/administration/sources/connections/connections';
+import  { Connections,IConnectionsProps } from 'app/modules/administration/sources/connections/connections';
 import userEvent from '@testing-library/user-event';
 import { DialogContainer, defaultTheme, Provider as SpectrumProvider } from '@adobe/react-spectrum';
 import { render } from '@testing-library/react';
@@ -11,9 +11,8 @@ import { MemoryRouter } from 'react-router';
 
 export const getInitialState = () => {
   return {
-    Sources: {
+    connections: {
       connections: [],
-      totalItems: 0,
     },
   };
 };
@@ -29,8 +28,8 @@ describe('Connections', () => {
       {
         id: 1,
         name: 'Postgres-connection',
-        connectionUsername: 'postgres',
-        connectionPassword: 'admin',
+        connectionUsername: 'test',
+        connectionPassword: 'test',
         connectionTypeId: 1,
         linkId: '1715917d-fff8-44a1-af02-ee2cd41a3609',
         realmId: 0,
@@ -40,106 +39,7 @@ describe('Connections', () => {
           serverIp: 'flair-pgsql',
           serverPort: '5432',
         },
-        connectionParameters: {},
-      },
-      {
-        id: 2,
-        name: 'Mysql-connection',
-        connectionUsername: 'root',
-        connectionPassword: 'admin',
-        connectionTypeId: 2,
-        linkId: '7bbdbca5-b704-4d78-9ad4-0d3e2458ee5f',
-        realmId: 0,
-        details: {
-          databaseName: 'services',
-          type: 'MySql',
-          serverIp: 'localhost',
-          serverPort: '3306',
-        },
-        connectionParameters: {},
-      },
-      {
-        id: 3,
-        name: 'Oracle-connection',
-        connectionUsername: 'system',
-        connectionPassword: 'oracle',
-        connectionTypeId: 3,
-        linkId: '58373090-6dbc-4635-b0ad-2c37c863b260',
-        realmId: 0,
-        details: {
-          databaseName: 'xe',
-          type: 'Oracle',
-          serverIp: 'localhost',
-          serviceName: 'xe',
-          serverPort: '1521',
-        },
-        connectionParameters: {},
-      },
-      {
-        id: 4,
-        name: 'Cockroachdb-connection',
-        connectionUsername: 'postgres',
-        connectionPassword: 'admin',
-        connectionTypeId: 6,
-        linkId: '1715917d-fff8-44a1-af02-ee2cd41a3611',
-        realmId: 0,
-        details: {
-          databaseName: 'services',
-          type: 'Cockroachdb',
-          serverIp: 'localhost',
-          serverPort: '26257',
-        },
-        connectionParameters: {},
-      },
-      {
-        id: 5,
-        name: 'Redshift-connection',
-        connectionUsername: 'vizcentric',
-        connectionPassword: 'Jengar1952',
-        connectionTypeId: 7,
-        linkId: '1715917d-fff8-44a1-af02-ee2cd41a3610',
-        realmId: 0,
-        details: {
-          databaseName: 'flairbi',
-          type: 'Redshift',
-          serverIp: 'flairbi-rs-instance.c0kvqfcudxql.eu-west-1.redshift.amazonaws.com',
-          serverPort: '5439',
-        },
-        connectionParameters: {},
-      },
-      {
-        id: 6,
-        name: 'Athena-connection',
-        connectionUsername: 'AKIAIBB4R46GJFLOZHJA',
-        connectionPassword: 'h6bhUXgjYB1gEa7KpU5WTbisx5Yt3Wp2+d/AuM96',
-        connectionTypeId: 8,
-        linkId: '1715917d-fff8-44a1-af02-ee2cd41a3123',
-        realmId: 0,
-        details: {
-          workgroup: 'athena-wg',
-          databaseName: 'centricdb',
-          type: 'Athena',
-          serverIp: 'athena.eu-west-1.amazonaws.com',
-          s3OutputLocation: 's3://aws-athena-query-results-689779572241-eu-west-1/',
-          serverPort: '443',
-        },
-        connectionParameters: {},
-      },
-      {
-        id: 7,
-        name: 'Kafka-connection',
-        connectionUsername: 'postgres',
-        connectionPassword: 'admin',
-        connectionTypeId: 9,
-        linkId: '1715917d-fff8-44a1-af02-ee2cd41a3010',
-        realmId: 0,
-        details: {
-          type: 'Kafka',
-          isSecure: 'false',
-          serverIp: 'localhost',
-          serverPort: '8088',
-        },
-        connectionParameters: {},
+        connectionParameters: { cacheEnabled: 'false', cachePurgeAfterMinutes: '0', refreshAfterMinutes: '0', refreshAfterTimesRead: '0' },
       },
     ],
     getConnections: jest.fn(),
