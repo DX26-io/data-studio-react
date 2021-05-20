849dd18da9a04ca948d50842c282de7a139ce138
dev-fe-dx26 <dev-fe-dx26>
2021-05-20 09:03:22 +0530

    fixed the test case

--- src/test/javascript/spec/app/modules/administration/user-management/permissions/users-groups.spec.tsx
+++ src/test/javascript/spec/app/modules/administration/user-management/permissions/users-groups.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UsersGroups,IUsersGroupsProps } from 'app/modules/administration/user-management/permissions/users-groups';
+import { UsersGroups, IUsersGroupsProps } from 'app/modules/administration/user-management/permissions/users-groups';
 import userEvent from '@testing-library/user-event';
 import { defaultTheme, Provider as SpectrumProvider } from '@adobe/react-spectrum';
 import { render } from '@testing-library/react';
@@ -13,8 +13,8 @@ import { AUTHORITIES } from 'app/config/constants';
 
 export const getInitialState = () => {
   return {
-    userGroups: { groups: [] },
-    userManagement: { users: [] },
+    userGroups: { groups: [], searchedGroups: [] },
+    userManagement: { users: [], searchedUsers: [] },
   };
 };
 
@@ -24,49 +24,67 @@ describe('Users Groups', () => {
   const permissionsProps = {
     history: { push: jest.fn() } as any,
     location: {} as any,
-    match: {} as any
+    match: {} as any,
   };
 
   const receivedProps = {
     getUserGroups: jest.fn(),
     getUsers: jest.fn(),
     searchUsers: jest.fn(),
-    searchUserGroups:jest.fn(),
+    searchUserGroups: jest.fn(),
     groups: [
-        {
-          id: '123',
-          name: 'test123',
-        },
-        {
-          id: '1234',
-          name: 'test1234',
-        },
-      ],
-      users: [
-        {
-          id: '123',
-          login: 'test1',
-          firstName: 'test1',
-          lastName: 'test1',
-          email: 'test1@dx26.com',
-          activated: true,
-          userGroups: [AUTHORITIES.ADMIN],
-          userType: 'xyz',
-        },
-        {
-          id: '113',
-          login: 'test2',
-          firstName: 'test2',
-          lastName: 'test2',
-          email: 'test2@dx26.com',
-          activated: true,
-          userGroups: [AUTHORITIES.ADMIN],
-          userType: 'xyz',
-        },
-      ]
-  }
+      {
+        id: '123',
+        name: 'test123',
+      },
+      {
+        id: '1234',
+        name: 'test1234',
+      },
+    ],
+    users: [
+      {
+        id: '123',
+        login: 'test1',
+        firstName: 'test1',
+        lastName: 'test1',
+        email: 'test1@dx26.com',
+        activated: true,
+        userGroups: [AUTHORITIES.ADMIN],
+        userType: 'xyz',
+      },
+      {
+        id: '113',
+        login: 'test2',
+        firstName: 'test2',
+        lastName: 'test2',
+        email: 'test2@dx26.com',
+        activated: true,
+        userGroups: [AUTHORITIES.ADMIN],
+        userType: 'xyz',
+      },
+    ],
+    searchedGroups: [
+      {
+        id: '123',
+        name: 'test123',
+      }
+    ],
+    searchedUsers: [
+      {
+        id: '123',
+        login: 'test1',
+        firstName: 'test1',
+        lastName: 'test1',
+        email: 'test1@dx26.com',
+        activated: true,
+        userGroups: [AUTHORITIES.ADMIN],
+        userType: 'xyz',
+      }
+    ],
+  };
 
-  const wrapper = (props : IUsersGroupsProps) => {
+  const wrapper = (props: IUsersGroupsProps) => {
     const mockStore = configureMockStore([thunk, promiseMiddleware]);
     const store = mockStore(getInitialState());
     if (!mountedWrapper) {
@@ -74,7 +92,7 @@ describe('Users Groups', () => {
         <SpectrumProvider theme={defaultTheme}>
           <Provider store={store}>
             <MemoryRouter>
-              <UsersGroups {...receivedProps} {...permissionsProps} {...props}/>
+              <UsersGroups {...receivedProps} {...permissionsProps} {...props} />
             </MemoryRouter>
           </Provider>
         </SpectrumProvider>
@@ -98,10 +116,9 @@ describe('Users Groups', () => {
     const tree = wrapper(null);
     const searchElement = tree.getByTestId('search');
     userEvent.click(searchElement);
-    userEvent.type(document.activeElement, "t");
-    expect(receivedProps.searchUsers.mock.calls.length).toEqual(2);
+    userEvent.type(document.activeElement, 'user');
+    expect(receivedProps.searchUsers.mock.calls.length).toEqual(3);
   });
 
   // TODO : need to find a way to test on change tab and list items
-
 });
