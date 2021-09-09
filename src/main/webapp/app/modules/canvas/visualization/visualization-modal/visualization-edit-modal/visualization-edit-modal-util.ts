1555a1e78daf5e85cad685862c7a80431dbc6c9e
dev-fe-dx26 <dev-fe-dx26>
2021-09-09 12:08:23 +0530

    change the design of data propperties

--- src/main/webapp/app/modules/canvas/visualization/visualization-modal/visualization-edit-modal/visualization-edit-modal-util.ts
+++ src/main/webapp/app/modules/canvas/visualization/visualization-modal/visualization-edit-modal/visualization-edit-modal-util.ts
@@ -22,7 +22,7 @@ export const getPropertiesTabTranslations = (): ITabData[] => {
     },
     {
       id: 'dataProperties',
-      name: translate('views.editConfiguration.properties.dataProperties'),
+      name: translate('views.editConfiguration.properties.dataProperties.title'),
     },
     // {
     //   id: 'hierarchy',
@@ -30,6 +30,24 @@ export const getPropertiesTabTranslations = (): ITabData[] => {
     // },
   ];
 };
+
+/**
+ * This method returns the list of data properies tab
+ */
+export const getDataPropertiesTabTranslations = (): ITabData[] => {
+  return [
+    {
+      id: 'DIMENSION',
+      name: translate('views.editConfiguration.properties.dataProperties.dimensions'),
+    },
+    {
+      id: 'MEASURE',
+      name: translate('views.editConfiguration.properties.dataProperties.measures'),
+    }
+  ];
+};
+
+
 /**
  * This method returns the list of border type
  */
