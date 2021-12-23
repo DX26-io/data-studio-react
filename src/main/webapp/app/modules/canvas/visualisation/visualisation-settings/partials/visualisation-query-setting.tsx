f647c34f9265b5ea4ee07535c2a2f4ed95ee8e65
dev-fe-dx26 <dev-fe-dx26>
2021-12-23 15:37:58 +0530

    fixed build issue-2

--- src/main/webapp/app/modules/canvas/visualisation/visualisation-settings/partials/visualisation-query-setting.tsx
+++ src/main/webapp/app/modules/canvas/visualisation/visualisation-settings/partials/visualisation-query-setting.tsx
@@ -3,12 +3,14 @@ import { View } from '@adobe/react-spectrum';
 import { IRootState } from 'app/shared/reducers';
 import { connect } from 'react-redux';
 
-
-const VisualisationQuerySetting = (props) => {
+const VisualisationQuerySetting = props => {
   return (
     <>
       <View>
-        <span className="query"> {props.validateQueryError ? props.validateQueryError : props.validateQueryResponse?.rawQuery}</span>
+        <span className="query">
+          {' '}
+          {props.validateQueryResponse?.error ? props.validateQueryResponse?.error : props.validateQueryResponse?.rawQuery}
+        </span>
       </View>
     </>
   );
@@ -16,7 +18,6 @@ const VisualisationQuerySetting = (props) => {
 
 const mapStateToProps = (storeState: IRootState) => ({
   validateQueryResponse: storeState.visualmetadata.validateQueryResponse,
-  validateQueryError: storeState.visualmetadata.validateQueryError,
 });
 
 type StateProps = ReturnType<typeof mapStateToProps>;
