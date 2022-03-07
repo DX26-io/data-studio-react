b2060258862cfe0bb71d0764a17e6a9d5b0c31ce
dev-fe-dx26 <dev-fe-dx26>
2022-03-07 15:04:04 +0530

    fixed the canvas header filter alignment issues

--- src/main/webapp/app/shared/layout/header/canvas-filter-header.tsx
+++ src/main/webapp/app/shared/layout/header/canvas-filter-header.tsx
@@ -6,17 +6,23 @@ import { connect } from 'react-redux';
 import { createEntity as addVisualmetadataEntity, toggleEditMode } from 'app/entities/visualmetadata/visualmetadata.reducer';
 import { saveViewState } from 'app/entities/views/views.reducer';
 import 'bootstrap/dist/css/bootstrap.css';
-import { applyFilter, addAppliedFilters, removeAppliedFilters, toggleFilterPanel, saveSelectedFilter, removeDateFilters } from 'app/modules/canvas/filter/filter.reducer';
+import {
+  applyFilter,
+  addAppliedFilters,
+  removeAppliedFilters,
+  toggleFilterPanel,
+  saveSelectedFilter,
+  removeDateFilters,
+} from 'app/modules/canvas/filter/filter.reducer';
 import { removeOptionFromFilters } from 'app/modules/canvas/filter/filter-util';
 import Close from '@spectrum-icons/workflow/Close';
 import Select from 'react-select';
 import { generateOptions } from 'app/shared/util/entity-utils';
 import { generateOptionsForDateRange, isDateFilterType, isDateRange } from 'app/modules/canvas/filter/filter-util';
 import { getFeature } from 'app/entities/feature/feature-util';
-export interface ICanvasFilterHeaderProps extends StateProps, DispatchProps { }
+export interface ICanvasFilterHeaderProps extends StateProps, DispatchProps {}
 
 const CanvasFilterHeader = (props: ICanvasFilterHeaderProps) => {
-
   const formatOptionLabel = ({ value, label }) => (
     <Flex direction="row" alignItems="center">
       <span className="spectrum-Body-emphasis--sizeXXS" style={{ marginRight: 'auto' }}>
@@ -29,31 +35,30 @@ const CanvasFilterHeader = (props: ICanvasFilterHeaderProps) => {
   const createdSelectedFilterElements = () => {
     const options = Object.keys(props.selectedFilters).map((featureName, i) => {
       return (
-        <View minWidth="size-2000" key={`filter-${featureName}`}>
-          {
-            isDateFilterType(props.selectedFilters[featureName]?._meta?.dataType) ? (
-              <>
-                <Select
-                  key={featureName}
-                  value={null}
-                  searchable={true}
-                  formatOptionLabel={formatOptionLabel}
-                  classNamePrefix="select"
-                  onChange={(value, actionMeta) => {
-                    if (actionMeta.action === 'select-option') {
-                      const feature = getFeature(props.featureList, featureName);
-                      if (feature && feature.dateFilter !== 'ENABLED') {
-                        props.removeDateFilters(props.selectedFilters, featureName);
-                        props.applyFilter(props.selectedFilters, props.visualmetadata, props.view);
-                      }
-
+        <React.Fragment  key={`header-filter-${featureName}`}>
+          {isDateFilterType(props.selectedFilters[featureName]?._meta?.dataType) ? (
+            <View minWidth="size-3000">
+              <Select
+                key={featureName}
+                value={null}
+                searchable={true}
+                formatOptionLabel={formatOptionLabel}
+                classNamePrefix="select"
+                onChange={(value, actionMeta) => {
+                  if (actionMeta.action === 'select-option') {
+                    const feature = getFeature(props.featureList, featureName);
+                    if (feature && feature.dateFilter !== 'ENABLED') {
+                      props.removeDateFilters(props.selectedFilters, featureName);
+                      props.applyFilter(props.selectedFilters, props.visualmetadata, props.view);
                     }
-                  }}
-                  placeholder={featureName}
-                  options={generateOptionsForDateRange(props.dynamicDateRangeMetaData[featureName])}
-                /></>
-
-            ) : (
+                  }
+                }}
+                placeholder={featureName}
+                options={generateOptionsForDateRange(props.dynamicDateRangeMetaData[featureName])}
+              />
+            </View>
+          ) : (
+            <View minWidth="size-2000">
               <Select
                 key={featureName}
                 value={null}
@@ -70,10 +75,9 @@ const CanvasFilterHeader = (props: ICanvasFilterHeaderProps) => {
                 placeholder={featureName}
                 options={generateOptions(props.selectedFilters[featureName])}
               />
-            )
-          }
-
-        </View>
+            </View>
+          )}
+        </React.Fragment>
       );
     });
     return options;
@@ -103,7 +107,7 @@ const mapDispatchToProps = {
   applyFilter,
   addAppliedFilters,
   removeAppliedFilters,
-  removeDateFilters
+  removeDateFilters,
 };
 
 type StateProps = ReturnType<typeof mapStateToProps>;
