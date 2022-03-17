4a56ea128f5ee76923c6de3162ceee83cc49c254
dev-fe-dx26 <dev-fe-dx26>
2022-03-17 16:36:20 +0530

    added filters in share link

--- src/main/webapp/app/entities/share/share-visualisation.tsx
+++ src/main/webapp/app/entities/share/share-visualisation.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Flex, View, } from '@adobe/react-spectrum';
+import { Flex, View } from '@adobe/react-spectrum';
 import { RouteComponentProps } from 'react-router-dom';
 import { IRootState } from 'app/shared/reducers';
 import { connect } from 'react-redux';
@@ -14,77 +14,98 @@ import { getConditionExpression } from 'app/modules/canvas/filter/filter-util';
 import { toggleFilterPanel } from 'app/modules/canvas/filter/filter.reducer';
 import FilterPanel from 'app/modules/canvas/filter/filter-panel';
 import { setIsShare } from './share-visualisation.reducer';
-
-export interface ISharevisualisationProps extends StateProps, DispatchProps, RouteComponentProps { }
+import { getViewFeaturesEntities } from 'app/entities/feature/feature.reducer';
+import {  getCurrentViewState } from 'app/entities/views/views.reducer';
+export interface ISharevisualisationProps extends StateProps, DispatchProps, RouteComponentProps {}
 
 const Sharevisualisation = (props: ISharevisualisationProps) => {
+  const params = new URLSearchParams(props.location.search);
+  const visualisationId = params.get('visualisationId');
+  const viewId = params.get('viewId');
+  const datasourceId = Number(params.get('datasourceId'));
 
-    const params = new URLSearchParams(props.location.search);
-    const visualisationId = params.get('visualisationId');
-    const viewId = params.get('viewId');
-    const datasourceId = Number(params.get('datasourceId'));
+  useEffect(() => {
+    props.setIsShare(true);
+    if (visualisationId) {
+      props.receiveSocketResponse();
+      props.getVisualmetadataEntity(visualisationId);
+      props.getViewEntity(viewId);
+      props.getViewFeaturesEntities(viewId);
+      props.getCurrentViewState(viewId);
+    }
+    return () => {
+      props.setIsShare(false);
+    };
+  }, []);
 
-    useEffect(() => {
-        props.setIsShare(true);
-        if (visualisationId) {
-            props.receiveSocketResponse();
-            props.getVisualmetadataEntity(visualisationId);
-            props.getViewEntity(viewId);
-        }
-        return () => {
-            props.setIsShare(false);
-        };
-    }, []);
-
-    useEffect(() => {
-        if (props.isvisualMetaDataFetched && props.visualmetadataEntity.fields && ValidateFields(props.visualmetadataEntity.fields)) {
-            props.receiveSocketResponseByVisualId(props.visualmetadataEntity.id);
-            const visualMetadata = VisualWrap(props.visualmetadataEntity);
-            const queryDTO = visualMetadata.getQueryParameters(props.visualmetadataEntity, {}, getConditionExpression({}), 0);
-            const body = {
-                queryDTO,
-                visualMetadata,
-                validationType: 'REQUIRED_FIELDS',
-                actionType: null,
-                type: 'share-link',
-            };
-            forwardCall(datasourceId, body, viewId);
-        }
-    }, [props.isvisualMetaDataFetched]);
-
-    useEffect(() => {
-        if (props.visualDataById) {
-            if (props.visualDataById?.data.length > 0) {
-                renderVisualisation(props.visualmetadataEntity, props.visualDataById?.data, 'visualisation-edit');
-            }
-        }
-    }, [props.visualDataById]);
-
-    return (
-        <>
-            <FilterPanel visualisationId={props.visualmetadataEntity.id} />
-            <Flex direction="column" flex gap="size-75">
-                <View id={`visualisation-edit-${props.visualmetadataEntity.id}`} height="90vh" >
-                </View>
-            </Flex>
-        </>
+const shareLinkfForwardCall = () => {
+    const visualMetadata = VisualWrap(props.visualMetadataEntity);
+    const queryDTO = visualMetadata.getQueryParameters(
+      props.visualMetadataEntity,
+      props.selectedFilters,
+      getConditionExpression(props.selectedFilters),
+      0
     );
+    const body = {
+      queryDTO,
+      visualMetadata,
+      validationType: 'REQUIRED_FIELDS',
+      actionType: null,
+      type: 'share-link',
+    };
+    forwardCall(datasourceId, body, viewId);
+  };
+
+  useEffect(() => {
+    if (props.isvisualMetaDataFetched && props.visualMetadataEntity.fields && ValidateFields(props.visualMetadataEntity.fields)) {
+      props.receiveSocketResponseByVisualId(props.visualMetadataEntity.id);
+      shareLinkfForwardCall();
+    }
+  }, [props.isvisualMetaDataFetched]);
+
+  useEffect(() => {
+    if (props.visualMetadataEntity.fields && ValidateFields(props.visualMetadataEntity.fields)) {
+      shareLinkfForwardCall();
+    }
+  }, [props.selectedFilters]);
+
+  useEffect(() => {
+    if (props.visualDataById) {
+      if (props.visualDataById?.data.length > 0) {
+        renderVisualisation(props.visualMetadataEntity, props.visualDataById?.data, 'visualisation-edit', props);
+      }
+    }
+  }, [props.visualDataById]);
+
+
+
+  return (
+    <>
+      <FilterPanel />
+      <Flex direction="column" flex gap="size-75">
+        <View id={`visualisation-edit-${props.visualMetadataEntity.id}`} height="90vh"></View>
+      </Flex>
+    </>
+  );
 };
 
 const mapStateToProps = (storeState: IRootState) => ({
-    visualmetadataEntity: storeState.visualmetadata.entity,
-    view: storeState.views.entity,
-    visualDataById: storeState.visualisationData.visualDataById,
-    isvisualMetaDataFetched: storeState.visualmetadata.isvisualMetaDataFetched,
+  visualMetadataEntity: storeState.visualmetadata.entity,
+  view: storeState.views.entity,
+  visualDataById: storeState.visualisationData.visualDataById,
+  isvisualMetaDataFetched: storeState.visualmetadata.isvisualMetaDataFetched,
+  selectedFilters: storeState.filter.selectedFilters,
 });
 
 const mapDispatchToProps = {
-    getVisualmetadataEntity,
-    getViewEntity,
-    receiveSocketResponseByVisualId,
-    toggleFilterPanel,
-    receiveSocketResponse,
-    setIsShare
+  getVisualmetadataEntity,
+  getViewEntity,
+  receiveSocketResponseByVisualId,
+  toggleFilterPanel,
+  receiveSocketResponse,
+  setIsShare,
+  getViewFeaturesEntities,
+  getCurrentViewState
 };
 
 type StateProps = ReturnType<typeof mapStateToProps>;
