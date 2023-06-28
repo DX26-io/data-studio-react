e9a27c223a4cf316e16f13dbb72fd2fca095d0d9
dev-fe-dx26 <dev-fe-dx26>
2023-06-28 14:28:55 +0530

    fixed build script

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 set -ex
 # SET THE FOLLOWING VARIABLES
 # sudo docker hub username
-USERNAME=dx26io
+PLATFORM=dx26io
 # image name
-IMAGE=data-studio
+PROJECT=data-studio
 # ensure we're up to date
 # git pull
 # bump version
@@ -11,14 +11,13 @@ sudo docker run --rm -v "$PWD":/app treeder/bump patch
 version=`cat VERSION`
 echo "version: $version"
 # run build
-./build.sh
+sudo docker build -t $PLATFORM/$PROJECT:"v$version" .
 # tag it
 git add VERSION
-git commit -m "version $version"
-git tag -a "v$version" -m "version $version"
-git push
-git push --tags
-sudo docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:"v$version"
+git commit -m "version v$version"
+# git tag -a "v$version" -m "version $version"
+git push origin tag "v$version"
+sudo docker tag $PLATFORM/$PROJECT:"v$version"
 # push it
-sudo docker push $USERNAME/$IMAGE:latest
-sudo docker push $USERNAME/$IMAGE:"v$version"
+sudo docker push $PLATFORM/$PROJECT:latest
+sudo docker push $PLATFORM/$PROJECT:"v$version"
