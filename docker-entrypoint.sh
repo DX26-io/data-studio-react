cdbb81ed1e51dc7f19013d43428d711ade2e5bb9
“johib” <johib.khan@gmail.com>
2023-10-26 20:20:55 +0530

    renamed variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${NGINX_PROXY} ${NGINX_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${GATEWAY_HOST} ${GATEWAY_API_PORT} ${GATEWAY_NETTY_SOCKET_IO_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
